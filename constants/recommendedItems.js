f3db5dd9486742d430a6a1a706feb9c610e6015e
Niladri <niladrisengit12@gmail.com>
2023-08-15 22:28:23 +0530

    day-05

---
+++ constants/recommendedItems.js
@@ -0,0 +1,29 @@
+export const recommendedItems = [
+    {
+      id: 1,
+      name: "Mocha Cookie Crumbelite",
+      description:
+        "Our Mocha Cookie crumble Latte is a deliciously flavoured mochaindulgence - Hot coffee meets chocolate and co",
+      price: 484,
+      imageSrc:
+        "https://images.unsplash.com/photo-1476224203421-9ac39bcb3327?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
+    },
+    {
+      id: 1,
+      name: "Mocha Cookie Crumbelite",
+      description:
+        "Our Mocha Cookie crumble Latte is a deliciously flavoured mochaindulgence - Hot coffee meets chocolate and co",
+      price: 484,
+      imageSrc:
+        "https://images.unsplash.com/photo-1563379926898-05f4575a45d8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
+    },
+    {
+      id: 1,
+      name: "Mocha Cookie Crumbelite",
+      description:
+        "Our Mocha Cookie crumble Latte is a deliciously flavoured mochaindulgence - Hot coffee meets chocolate and co",
+      price: 484,
+      imageSrc:
+        "https://images.unsplash.com/photo-1519708227418-c8fd9a32b7a2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
+    },
+  ];
